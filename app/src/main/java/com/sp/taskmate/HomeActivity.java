37075972acbaff2c87f6cb5028e58df6a0b24594
Hong Yi <leehy.23@ichat.sp.edu.sg>
2025-01-19 22:30:11 +0800

    home screen commit 1

---
+++ app/src/main/java/com/sp/taskmate/HomeActivity.java
@@ -0,0 +1,12 @@
+package com.sp.taskmate;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class HomeActivity extends AppCompatActivity {
+
+}
