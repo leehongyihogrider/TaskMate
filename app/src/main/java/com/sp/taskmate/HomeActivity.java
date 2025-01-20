82dc53ce83e10eefcc9956d053a84b4141c85df8
Kaden <KADEN.23@ichat.sp.edu.sg>
2025-01-20 12:45:52 +0800

    213

--- app/src/main/java/com/sp/taskmate/HomeActivity.java
+++ app/src/main/java/com/sp/taskmate/HomeActivity.java
@@ -2,11 +2,11 @@ package com.sp.taskmate;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
 public class HomeActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
 }
